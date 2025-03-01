be7ea5422b9116a04a8725a22eab4c55e2e068ac
Yannick <tech@yalit.be>
2025-03-01 18:50:06 +0100

    fix : fix useCategories

--- assets/scripts/hooks/library/useCategories.tsx
+++ assets/scripts/hooks/library/useCategories.tsx
@@ -1,29 +1,30 @@
-import {useQuery} from "react-query";
-import {fetchCategories,} from "../../repository/library.repository";
-import {useState} from "react";
-import {ScoreCategory} from "../../model/library/scoreCategory.interface";
+import { useQuery, UseQueryResult } from "react-query";
+import { fetchCategories } from "../../repository/library.repository";
+import { useState } from "react";
+import { ScoreCategory } from "../../model/library/scoreCategory.interface";
+import { ScoreCategoryCollectionOutput } from "assets/scripts/repository/collectionOutput.interface";
 
 interface AllCategoriesOutput {
-    categories: ScoreCategory[],
-    fetchData: {
-        value: string,
-        set: (v: string) => void
-    }
+  categories: ScoreCategory[];
+  fetchData: {
+    value: string;
+    set: (v: string) => void;
+  };
 }
 
 export function useCategories(): AllCategoriesOutput {
-    const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
 
-    const query = useQuery({
-        queryKey: ["allScoreCategories", value],
-        queryFn: async () => fetchCategories(value)
-    })
+  const query: UseQueryResult<ScoreCategoryCollectionOutput> = useQuery({
+    queryKey: ["allScoreCategories", value],
+    queryFn: async () => fetchCategories(value),
+  });
 
-    return {
-        categories: query.data ?? [],
-        fetchData: {
-            value,
-            set: (v: string = '') => setValue(v.trim()),
-        }
-    }
+  return {
+    categories: query.data?.data ?? [],
+    fetchData: {
+      value,
+      set: (v: string = "") => setValue(v.trim()),
+    },
+  };
 }
--- assets/scripts/shadcdn/components/ui/multipleSelector.tsx
+++ assets/scripts/shadcdn/components/ui/multipleSelector.tsx
@@ -1,55 +1,71 @@
-import {Command, CommandEmpty, CommandGroup, CommandItem, CommandList, CommandShortcut} from "./command";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+  CommandShortcut,
+} from "./command";
 
-import {Command as CommandPrimitive} from "cmdk"
-import {useEffect, useState} from "react";
-import {Badge} from "./badge";
-import {XMarkIcon} from "@heroicons/react/24/outline";
+import { Command as CommandPrimitive } from "cmdk";
+import { useEffect, useState } from "react";
+import { Badge } from "./badge";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
 type MultipleSelectorProps<T extends object> = {
-    values: T[],
-    setValue: (value: T[]) => void,
-    displayValue: (v: T) => string,
-    idValue: (v: T) => string,
-    possibleValues: T[]
-}
+  values: T[];
+  setValue: (value: T[]) => void;
+  displayValue: (v: T) => string;
+  idValue: (v: T) => string;
+  possibleValues: T[];
+};
 export default function MultipleSelector<T extends object>({
-                                                               values,
-                                                               setValue,
-                                                               displayValue, idValue,
-                                                               possibleValues,
-                                                           }: MultipleSelectorProps<T>) {
-    const [selectedValues, setSelectedValues] = useState<T[]>(values)
-    const [open, setOpen] = useState<boolean>(false)
+  values,
+  setValue,
+  displayValue,
+  idValue,
+  possibleValues,
+}: MultipleSelectorProps<T>) {
+  const [selectedValues, setSelectedValues] = useState<T[]>(values);
+  const [open, setOpen] = useState<boolean>(false);
 
-    const removeValue = (property: string): void => {
-        setSelectedValues(selectedValues.filter(value => idValue(value) !== property));
-    }
+  const removeValue = (property: string): void => {
+    setSelectedValues(
+      selectedValues.filter((value) => idValue(value) !== property),
+    );
+  };
 
-    useEffect(() => {
-        onChange(selectedValues);
-    }, [selectedValues]);
+  useEffect(() => {
+    setValue(selectedValues);
+  }, [selectedValues]);
 
-    return (
-        <Command>
-            <div className="min-h-10 text-base px-3 md:text-sm flex items-center gap-2">
-                {values.map((v: T) => (
-                    <Badge key={idValue(v)}>
-                        <span>{displayValue(v)}</span>
-                        <XMarkIcon className="h-4 w-4 cursor-pointer" onClick={() => removeValue(idValue(v))}/>
-                    </Badge>))}
-                <CommandPrimitive.Input
-                    className="flex h-10 flex-1 w-full border-b bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
-                    onFocus={() => setOpen(!open)}
-                />
-            </div>
-            {open && (
-                <CommandList>
-                    <CommandEmpty>No results found.</CommandEmpty>
-                    {possibleValues.map((p: T) => (
-                        <CommandItem><span>{displayValue(p)}</span></CommandItem>
-                    ))}
-                </CommandList>
-            )}
-        </Command>
-    )
+  return (
+    <Command>
+      <div className="min-h-10 text-base px-3 md:text-sm flex items-center gap-2">
+        {values.map((v: T) => (
+          <Badge key={idValue(v)}>
+            <span>{displayValue(v)}</span>
+            <XMarkIcon
+              className="h-4 w-4 cursor-pointer"
+              onClick={() => removeValue(idValue(v))}
+            />
+          </Badge>
+        ))}
+        <CommandPrimitive.Input
+          className="flex h-10 flex-1 w-full border-b bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
+          onFocus={() => setOpen(!open)}
+        />
+      </div>
+      {open && (
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {possibleValues.map((p: T) => (
+            <CommandItem>
+              <span>{displayValue(p)}</span>
+            </CommandItem>
+          ))}
+        </CommandList>
+      )}
+    </Command>
+  );
 }
