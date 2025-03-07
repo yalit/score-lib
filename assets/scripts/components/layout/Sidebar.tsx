65aac7e69e4347ab30db18273954b272a9e63649
Yannick <tech@yalit.be>
2025-03-07 18:06:41 +0100

    feat : make sidebar collapsible

--- assets/scripts/components/layout/Sidebar.tsx
+++ assets/scripts/components/layout/Sidebar.tsx
@@ -1,8 +1,8 @@
 import useRouter from "../../hooks/useRouter";
-import {useTranslation} from "react-i18next";
-import {useSecurity} from "../../context/security/security.hooks";
+import {useTranslator} from "../../hooks/useTranslator";
+import {SquareLibraryIcon} from "lucide-react";
 import {
-    AdjustmentsVerticalIcon, ArrowRightStartOnRectangleIcon,
+    ArrowRightStartOnRectangleIcon,
     Bars3BottomRightIcon,
     BuildingLibraryIcon,
     HomeModernIcon,
@@ -10,46 +10,88 @@ import {
 } from "@heroicons/react/24/outline";
 
 import '../../../styles/sidebar.css';
-import {AdjustmentsHorizontalIcon} from "@heroicons/react/16/solid";
-import {useTranslator} from "../../hooks/useTranslator";
+import {ReactNode, useMemo, useState} from "react";
+import {cn} from "../../shadcdn/lib/utils";
+import {useSecurity} from "../../context/security/security.hooks";
+
+export type MenuItemProps = {
+    title: string;
+    icon?: ReactNode;
+    path: string;
+    visible?: () => boolean;
+}
 
 export default function Sidebar() {
+    const [titleDisplayed, setTitleDisplayed] = useState(true);
+    const [mobileLinksShown, setMobileLinksShown] = useState(false);
+
     const {generate} = useRouter();
     const {trans} = useTranslator();
-    const {isGranted, isLogged, user} = useSecurity();
+    const {isLogged, user} = useSecurity();
 
     const iconClass = "w-6 h-6";
 
+    const menuItems: MenuItemProps[] = [
+        {
+            title: trans('main.menu.home.label'),
+            icon: <HomeModernIcon className={iconClass}/>,
+            path: generate('app_index')
+        },
+        {
+            title: trans('main.menu.library.label'),
+            icon: <BuildingLibraryIcon className={iconClass}/>,
+            path: generate('app_library_index')
+        },
+        {title: trans('main.menu.setlists.label'), icon: <ListBulletIcon className={iconClass}/>, path: "#"},
+        {
+            title: `${trans('main.menu.logout.label')} ${(user?.name)}`,
+            icon: <ArrowRightStartOnRectangleIcon className={iconClass}/>,
+            path: generate("app_logout"),
+            visible: isLogged
+        },
+    ]
+
+    const menuListClassname: string = useMemo<string>(() => {
+        return cn(
+            "navigation items-center flex-col md:items-start font-bold gap-3 p-5 bg-inherit",
+            mobileLinksShown ? "flex" : "hidden md:flex",
+            mobileLinksShown ? "absolute top-full z-50 inset-x-0" : ""
+        )
+
+    }, [mobileLinksShown])
+
+    const titleSpanClassname: string = useMemo<string>(() => {
+        return cn(
+            titleDisplayed ? "block" : "md:hidden",
+        )
+    }, [titleDisplayed])
+
     return (
         <div
-            className="navigation bg-gray-900 relative md:min-h-screen w-screen md:w-3/12 md:max-w-max flex justify-between items-center md:justify-start md:items-start md:block">
-            <div className="site--title text-white text-center font-bold p-7 text-2xl">{trans('main.title.label')} </div>
+            className="navigation bg-gray-900 text-white relative md:min-h-screen w-screen md:w-3/12 md:max-w-max flex justify-between items-center md:justify-start md:items-start md:block">
+            <button
+                className="flex items-center gap-2 site--title text-center font-bold px-5 py-7 text-2xl cursor-pointer"
+                onClick={() => setTitleDisplayed(!titleDisplayed)}>
+                <SquareLibraryIcon className={iconClass}/>
+                <span className={cn(titleSpanClassname)}> {trans('main.title.label')}</span>
+            </button>
 
-            <label htmlFor="nav-toggle" className="md:hidden p-5 cursor-pointer text-white">
-                <Bars3BottomRightIcon className={iconClass} />
-            </label>
-            <input type="checkbox" id="nav-toggle" className="peer hidden"/>
+            <div className="md:hidden p-5 cursor-pointer">
+                <Bars3BottomRightIcon className={iconClass} onClick={() => setMobileLinksShown(!mobileLinksShown)}/>
+            </div>
             <nav
-                className="navigation hidden peer-checked:flex peer-checked:absolute peer-checked:top-full peer-checked:z-50 peer-checked:inset-x-0 justify-center items-center flex-col md:justify-start md:block font-bold gap-3 text-white p-5 bg-inherit">
-                <a href={generate('app_index')} className="nav__menu__item">
-                    <HomeModernIcon className={iconClass} />
-                    <span>{trans('main.menu.home.label')}</span>
-                </a>
-                <a href={generate('app_library_index')} className="nav__menu__item">
-                    <BuildingLibraryIcon className={iconClass} />
-                    <span>{trans('main.menu.library.label')} </span>
-                </a>
-                <a href="#" className="nav__menu__item">
-                    <ListBulletIcon className={iconClass} />
-                    <span>{trans('main.menu.setlists.label')}</span>
-                </a>
-
-                {isLogged() &&
-                    <a href={generate("app_logout")} className="nav__menu__item">
-                        <ArrowRightStartOnRectangleIcon className={iconClass} />
-                        <span>{trans('main.menu.logout.label')} ({ user?.name })</span>
-                    </a>
-                }
+                className={menuListClassname}>
+                {menuItems.map(({path, icon, title, visible}: MenuItemProps) => {
+                    const isDisplayed = !visible || visible();
+                    return (
+                        <>{isDisplayed && (
+                            <a href={path} className="nav__menu__item" key={title}>
+                                {icon && icon}
+                                <span className={cn(titleSpanClassname)}>{title}</span>
+                            </a>
+                        )}</>
+                    )
+                })}
             </nav>
         </div>
     )
