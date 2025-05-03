14dbeb6f4e7345a62a3858290bdcea516caeeedf
Yannick <tech@yalit.be>
2025-05-04 01:25:00 +0200

    refactor : refactor actions for libraryTable.tsx

--- assets/scripts/components/table/sortableTableHead.tsx
+++ assets/scripts/components/table/sortableTableHead.tsx
@@ -7,17 +7,20 @@ import {
 import {SortableItem, sortContext} from "../../context/global/sortContext";
 import {TableHead} from "../../shadcdn/components/ui/table";
 import SortingIcon from "./sortingIcon";
+import {ScoreTableDataContext} from "../../context/library/scoreTableDataContext";
 
 type SortableTableHeadProps = PropsWithChildren & { sortItem: SortableItem };
 export default function SortableTableHead({
                                               children,
                                               sortItem,
                                           }: SortableTableHeadProps) {
+    const {state: {canSort}} = useContext(ScoreTableDataContext)
+
     return (
         <TableHead>
             <div className="flex justify-between">
                 <div>{children}</div>
-                <SortingIcon sortItem={sortItem}/>
+                {canSort && <SortingIcon sortItem={sortItem}/>}
             </div>
         </TableHead>
     );
