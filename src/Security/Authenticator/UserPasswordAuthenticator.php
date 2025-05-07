9e0316bd46828a1f530995324de0ac8888f730b2
Yannick <tech@yalit.be>
2025-05-07 10:32:17 +0200

    feat : add custom authenticator to allow to catch redirect to api calls

---
+++ src/Security/Authenticator/UserPasswordAuthenticator.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Security\Authenticator;
+
+
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class UserPasswordAuthenticator extends AbstractAuthenticator
+{
+    use TargetPathTrait;
+    public const LOGIN_ROUTE = 'app_login';
+
+    public function __construct(private readonly UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return self::LOGIN_ROUTE === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $username = $request->request->get('_username', '');
+        $password = $request->request->get('_password', '');
+
+        return new Passport(
+            new UserBadge($username),
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        $defautTargetPath = $this->urlGenerator->generate('app_index');
+
+        $targetPath = $this->getTargetPath($request->getSession(), $firewallName);
+        if (!$targetPath) {
+            return null; // continue as if nothing happened... ;-)
+        }
+
+        $parts = explode('/', $targetPath);
+        // redirect to default path if api
+        // api urls are like this : http?s://<domain>/api/...
+        if ($parts[3] === 'api') {
+            return new RedirectResponse($defautTargetPath);
+        }
+
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new RedirectResponse('/login');
+    }
+}
