ebcd6b3922c8f553685b409d3f20a8ee95c34cb6
Yannick <tech@yalit.be>
2025-05-07 11:18:53 +0200

    feat : add "Add" button

--- assets/scripts/pages/library/Library.tsx
+++ assets/scripts/pages/library/Library.tsx
@@ -5,8 +5,13 @@ import {LibraryTableDataProvider} from "../../context/library/LibraryTableDataPr
 import useLibraryPathInformation from "../../hooks/library/useLibraryPathInformation";
 import {useEffect, useState} from "react";
 import LibraryTable from "../../components/library/libraryTable";
+import {LibraryFilters} from "../../components/library/libraryFilters";
+import {PlusIcon} from "lucide-react";
+import {Button} from "../../shadcdn/components/ui/button";
+import useRouter from "../../hooks/useRouter";
 
 export default function Library() {
+    const {generate} = useRouter()
     const path = useLibraryPathInformation()
     const [searchData, setSearchData] = useState<string|null>(null);
 
@@ -21,6 +26,12 @@ export default function Library() {
             <Card>
                 <CardContent>
                     <LibraryTableDataProvider searchData={searchData}>
+                        <div className="flex justify-between items-start">
+                            <LibraryFilters />
+                            <a href={generate('app_library_score_new')}>
+                                <Button type="button" variant="outline"><PlusIcon/>Ajouter</Button>
+                            </a>
+                        </div>
                         <LibraryTable />
                     </LibraryTableDataProvider>
                 </CardContent>
