320eedb0b9fbb456e4179f579595241ffca5e5b1
Yannick <tech@yalit.be>
2025-05-07 11:44:36 +0200

    feat : update dashboard with listing number

--- assets/scripts/pages/home/DashboardHeader.tsx
+++ assets/scripts/pages/home/DashboardHeader.tsx
@@ -10,17 +10,16 @@ export default function DashboardHeader() {
     const {nbScores, nbLists, nbCreatedInLastWeek} = useLibraryStat()
 
     return (
-        <div className="main__cards w-full grid grid-cols-1 gap-3 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:gap-7.5">
-            <ActionCard title={trans('index.cards.newscores.title')} subtitle={String(nbCreatedInLastWeek)}/>
+        <div className="main__cards w-full grid grid-cols-1 gap-3 sm:gap-6 sm:grid-cols-2 2xl:gap-7.5">
             <ActionCard title={trans('index.cards.libraryscores.title')} subtitle={String(nbScores)}
                         Icon={<FilePlusIcon className="h-10 w-10"/>}
                         iconText={trans('index.cards.libraryscores.icon.action.label')}
                         actionPath={generate('app_library_score_new')}
             />
-            <ActionCard title={trans('index.cards.setlists.title')} subtitle={'n.a.'}
+            <ActionCard title={trans('index.cards.setlists.title')} subtitle={String(nbLists)}
                         Icon={<FolderPlusIcon className="h-10 w-10"/>}
-                        //iconText={trans('index.cards.setlists.icon.action.label')}
-                        //actionPath={generate('app_index')}
+                        iconText={trans('index.cards.setlists.icon.action.label')}
+                        actionPath={generate('app_listing_new')}
             />
         </div>
     )
--- src/Library/API/Provider/LibraryStatProvider.php
+++ src/Library/API/Provider/LibraryStatProvider.php
@@ -6,20 +6,24 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Library\API\DTO\LibraryStat;
 use App\Library\Repository\ScoreRepository;
+use App\Listing\Repository\ListingRepository;
 
 /**
  * @implements ProviderInterface<LibraryStat>
  */
 final readonly class LibraryStatProvider implements ProviderInterface
 {
-    public function __construct(private ScoreRepository $scoreRepository)
-    {
+    public function __construct(
+        private ScoreRepository $scoreRepository,
+        private ListingRepository $listingRepository,
+    ) {
     }
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object
     {
         $nbScores = count($this->scoreRepository->findAll());
         $nbScoresInLastSevenDays = $this->scoreRepository->findAllScoresInLastSevenDays();
-        return new LibraryStat($nbScores, 0, $nbScoresInLastSevenDays);
+        $nbLists = count($this->listingRepository->findAll());
+        return new LibraryStat($nbScores, $nbLists, $nbScoresInLastSevenDays);
     }
 }
